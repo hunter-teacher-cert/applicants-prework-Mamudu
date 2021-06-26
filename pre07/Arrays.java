ac8a22b418a48455dfffd7b29b0cda9025ee3b62
Mamudu <42047513+Mamudu@users.noreply.github.com>
2021-06-26 01:51:04 +0000

    just started

---
+++ pre07/Arrays.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class Arrays{
+  public static void main(String[]args){
+  System.out.println(powArray());
+}
+public static double powArray(){  
+    double a[]= new double[5];
+      for (int i=0; i<a.length; i++){
+        a[i]=a[i*i];
+        double b= a[i];
+     }
+  } return a[];
+}
