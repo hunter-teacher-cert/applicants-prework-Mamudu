9cfab4c2b5b5ffa13e9b6544baac45d5a78a551d
Mamudu <42047513+Mamudu@users.noreply.github.com>
2021-06-28 02:59:20 +0000

    Hello

--- preo01/Hello.java
+++ preo01/Hello.java
@@ -1,8 +1,8 @@
 import java.io.*;
 import java.util.*;
 
-public class hello {
+public class Hello {
 	public static void main(String[] args){
-			System.out.println("Hello world!");
+		System.out.println("Hello world!");
 	}
 }
